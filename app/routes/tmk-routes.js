c929682cf02e56efaa3dea999212ebe4558d3ff3
Alestainer <nikolaev.anton@phystech.edu>
2018-04-26 16:41:05 -1000

    POST request to put permits into mongodb

--- app/routes/tmk-routes.js
+++ app/routes/tmk-routes.js
@@ -8,7 +8,6 @@ const ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
   app.get('/tmks', (req, res) => {
     const num = parseInt(req.params.num, 10);
-    // TODO Add date difference
     db.collection('tmks').find({ $or: [{parsed: false}, {lastParsed: {$lte: parsingDeadline}}]}, (err, tmks) => {
       if (err) {
         res.send({'error': 'An error has occurred'});
@@ -26,24 +25,25 @@ module.exports = function(app, db) {
 
   app.post('/tmks/:id', (req, res) => {
     const id = req.params.id;
-    const note = { lastParsed: new Date(), parsed: req.body.body};
-    const details = {'_id': new ObjectID(id)};
-    db.collection('tmks').update(details, note, {upsert: true}, (err) => {
+    console.log('Inside tmk POST');
+    const note = {tmk: id, parsed: req.body.body, lastParsed: new Date() };
+    // const details = {'_id': new ObjectID(id)};
+    db.collection('tmks').update({tmk: id}, note, {upsert: true}, (err) => {
       if (err) {
         res.send({'error': 'An error has occurred: ', err});
       } else {
-        res.send(`Object ${details._id} is processed`);
+        res.send(`Object ${note.tmk} is processed`);
 	    }
     });
   });
 
-  app.post('/permits', (req, res) => {
-    const permit = { permit: req.body.body, appNumber: req.params.appNumber };
-    db.collection('permits').update({'appNumber': permit.appNumber}, permit, {upsert: true}, (err, result) => {
+  app.post('/permits/:appnumber', (req, res) => {
+    const permit = { appNumber: req.params.appnumber, permit: req.body.body};
+    db.collection('permits').update({appNumber: permit.appNumber}, permit, {upsert: true}, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
       } else {
-        res.send(result.ops[0]);
+        res.send(result);
       }
     });
   });
